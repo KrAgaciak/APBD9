2ec3aa5261c4803c132ef6dbfd9e5593df48e7e5
KrAgaciakStudenc <s28585@pjwstk.edu.pl>
2024-05-28 14:51:41 +0200

    Late Initial Commit

---
+++ APBD9/Controllers/TripsControllers.cs
@@ -0,0 +1,25 @@
+﻿using APBD9.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD9.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TripsControllers: ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public TripsControllers(ApbdContext _context)
+    {
+        this._context = _context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        
+        
+        return Ok();
+    }
+
+}
