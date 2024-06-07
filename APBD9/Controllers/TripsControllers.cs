5c7fd9ade181d9a9d8bcf9981dc3f1fb43a76ca4
Krzysztof <k.agaciak@gmail.com>
2024-06-07 20:21:54 +0200

    Added Get and delete

--- APBD9/Controllers/TripsControllers.cs
+++ APBD9/Controllers/TripsControllers.cs
@@ -1,5 +1,6 @@
 ﻿using APBD9.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APBD9.Controllers;
 
@@ -17,9 +18,27 @@ public class TripsControllers: ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetTrips()
     {
-        
-        
-        return Ok();
+        // Implementation of fetching trips without sorting and pagination
+        var trips = await _context.Trips.ToListAsync();
+        return Ok(trips);
+    }
+
+    // New GET method with sorting and pagination
+    [HttpGet("trips")]
+    public async Task<IActionResult> GetTripsSortedAndPaged(int? page, int pageSize = 10)
+    {
+        // Calculate the number of items to skip
+        int pageNumber = page ?? 1;
+        int skipAmount = (pageNumber - 1) * pageSize;
+
+        // Fetch trips sorted by start date in descending order with pagination
+        var trips = await _context.Trips
+            .OrderByDescending(t => t.DateFrom)
+            .Skip(skipAmount)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return Ok(trips);
     }
 
 }
